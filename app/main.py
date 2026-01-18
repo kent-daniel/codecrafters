e5bd4d09781c137caeb04719eee0c3a13e2c507c
kent-daniel <kentdaniel0328@gmail.com>
2026-01-18 19:28:02 +1100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,18 +1,38 @@
+import os
 import sys
 
 BUILTIN_COMMANDS = {
     "echo": lambda args: sys.stdout.write(' '.join(args) + '\n'),
-    "type": lambda args: sys.stdout.write(f'{args[0]} is a shell builtin\n') if args and args[0] in BUILTIN_COMMANDS else sys.stdout.write(f'{args[0]}: not found\n'),
+    "type": lambda args: type_function(args),
     "exit": lambda args: sys.exit(0),
 }
 
+
+def type_function(args):
+    if not args:
+        return
+    command = args[0]
+    if command in BUILTIN_COMMANDS:
+        sys.stdout.write(f"{command} is a shell builtin\n")
+        return
+    elif 'PATH' in os.environ:
+        paths = os.environ['PATH'].split(os.pathsep)
+        for path in paths:
+            full_path = os.path.join(path, command)
+            if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                sys.stdout.write(f"{command} is {full_path}\n")
+                return
+    sys.stdout.write(f"{command}: not found\n")
+
 def main():
-    # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
-        command, *args = input().split()
+        parts = input().split()
+        if not parts:
+            continue
+        command, *args = parts
 
-        if command in BUILTIN_COMMANDS.keys():
+        if command in BUILTIN_COMMANDS:
             BUILTIN_COMMANDS[command](args)
         else:
             sys.stdout.write('{}: command not found\n'.format(command))
