e8177245f2b8d7d657ef6160a6ff4415071699b8
kent-daniel <kentdaniel0328@gmail.com>
2026-01-18 18:37:08 +1100

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    # sys.stdout.write("$ ")
-    pass
+    sys.stdout.write("$ ")
+    # pass
 
 
 if __name__ == "__main__":
