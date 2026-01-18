00975e37185bc0370339797edb22af76c398de74
kent-daniel <kentdaniel0328@gmail.com>
2026-01-18 19:15:04 +1100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
 import sys
 
+BUILTIN_COMMANDS = {
+    "echo": lambda args: sys.stdout.write(' '.join(args) + '\n'),
+    "type": lambda args: sys.stdout.write(f'{args[0]} is a shell builtin\n') if args and args[0] in BUILTIN_COMMANDS else sys.stdout.write(f'{args[0]}: not found\n'),
+    "exit": lambda args: sys.exit(0),
+}
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
-        command = input()
+        command, *args = input().split()
 
-        if command == "exit":
-            break
-        elif command.startswith("echo"):
-            sys.stdout.write(command[5:] + '\n')
+        if command in BUILTIN_COMMANDS.keys():
+            BUILTIN_COMMANDS[command](args)
         else:
             sys.stdout.write('{}: command not found\n'.format(command))
 
