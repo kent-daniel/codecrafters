5e824fbc5d00728ad5446645bbc5d56b1232b8a8
kent-daniel <kentdaniel0328@gmail.com>
2026-01-18 19:39:38 +1100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 
 BUILTIN_COMMANDS = {
@@ -8,21 +9,25 @@ BUILTIN_COMMANDS = {
 }
 
 
+def find_executable(command):
+    if 'PATH' not in os.environ:
+        return None
+    for path in os.environ['PATH'].split(os.pathsep):
+        full_path = os.path.join(path, command)
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            return full_path
+    return None
+
 def type_function(args):
     if not args:
         return
     command = args[0]
     if command in BUILTIN_COMMANDS:
         sys.stdout.write(f"{command} is a shell builtin\n")
-        return
-    elif 'PATH' in os.environ:
-        paths = os.environ['PATH'].split(os.pathsep)
-        for path in paths:
-            full_path = os.path.join(path, command)
-            if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-                sys.stdout.write(f"{command} is {full_path}\n")
-                return
-    sys.stdout.write(f"{command}: not found\n")
+    elif path := find_executable(command):
+        sys.stdout.write(f"{command} is {path}\n")
+    else:
+        sys.stdout.write(f"{command}: not found\n")
 
 def main():
     while True:
@@ -34,6 +39,8 @@ def main():
 
         if command in BUILTIN_COMMANDS:
             BUILTIN_COMMANDS[command](args)
+        elif find_executable(command):
+            subprocess.run([command] + args)
         else:
             sys.stdout.write('{}: command not found\n'.format(command))
 
